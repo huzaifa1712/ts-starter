37c04cb8254c01326ed38c07679e6d5538ae5059
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2023-08-02 22:44:25 +0800

    Modify test

--- test/main.ts
+++ test/main.ts
@@ -1,6 +1,8 @@
 import { test } from 'node:test';
 import * as assert from 'node:assert';
 
+import { add } from '../src/fn';
+
 test("if 2+2 is 4", () => {
-    assert.equal(2+2,4);
+    assert.equal(add(2,2), 4);
 });
